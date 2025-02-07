0e98a16e009fce3450c3928fd97b17c64c50231d
KEYING <101945582+whykeyday@users.noreply.github.com>
2025-02-06 20:47:53 -0500

    update

--- lab2/sketch.js
+++ lab2/sketch.js
@@ -2,7 +2,7 @@ function checkCollision(obj1, obj2) {
     return obj1.x < obj2.x + obj2.w &&
            obj1.x + obj1.w > obj2.x &&
            obj1.y < obj2.y + obj2.h &&
-           obj1.y + obj1.h > obj2.y;
+           obj1.y + obj2.h > obj2.y;
 }
 
 function checkCircleCollision(obj1, obj2) {
@@ -34,55 +34,129 @@ function handleCollisionStar(obj1, obj2) {
 }
 
 var redBrick = {
-    x: 0, y: 0, w: 50, h: 50, xSpeed: 8, ySpeed: 8, colour: 'red',
-    draw: function() { fill(this.colour); rect(this.x, this.y, this.w, this.h); },
-    move: function() {
-        this.x += this.xSpeed;
-        this.y += this.ySpeed;
-        if (this.x > width - this.w || this.x < 0) this.xSpeed *= -1;
-        if (this.y > height - this.h || this.y < 0) this.ySpeed *= -1;
-    }
-};
-
-var blueBrick = {
-    x: 1080, y: 0, w: 60, h: 60, xSpeed: 5, ySpeed: 5, colour: "#9ACDFF",
+    x: 0,
+    y: 0,
+    w: 40, // Doubled the width
+    h: 40, // Doubled the height
+    xSpeed: 4,
+    ySpeed: 5,
+    colour: 'red',
     draw: function() {
         fill(this.colour);
-        if (this.colour === 'yellow') ellipse(this.x + this.w / 2, this.y + this.h / 2, this.w, this.h);
-        else rect(this.x, this.y, this.w, this.h);
+        rect(this.x, this.y, this.w, this.h);
     },
     move: function() {
         this.x += this.xSpeed;
         this.y += this.ySpeed;
-        if (this.x > width - this.w || this.x < 0) this.xSpeed *= -1;
-        if (this.y > height - this.h || this.y < 0) this.ySpeed *= -1;
-        this.colour = (this.x > width / 2) ? 'yellow' : "#9ACDFF";
+
+        if (this.x > width - this.w || this.x < 0) {
+            this.xSpeed *= -1;
+        }
+        if (this.y > height - this.h || this.y < 0) {
+            this.ySpeed *= -1;
+        }
+    }
+};
+
+var blueBrick = {
+    x: 780,
+    y: 700,
+    w: 60, // Doubled the width
+    h: 60, // Doubled the height
+    xSpeed: -2,
+    ySpeed: 1,
+    colour: "#9ACDFF",
+    draw: function() {
+        fill(this.colour);
+        if (this.colour === 'yellow') {
+            ellipse(this.x + this.w / 2, this.y + this.h / 2, this.w, this.h);
+        } else {
+            rect(this.x, this.y, this.w, this.h);
+        }
+    },
+    move: function() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if (this.x > width - this.w || this.x < 0) {
+            this.xSpeed *= -1;
+        }
+        if (this.y > height - this.h || this.y < 0) {
+            this.ySpeed *= -1;
+        }
+        if (this.x > width / 2) {
+            this.colour = 'yellow';
+        } else {
+            this.colour = "#9ACDFF";
+        }
     }
 };
 
 var star = {
-    x: windowWidth / 3, y: windowHeight / 3, w: 70, h: 70, colour: 'white', stroke: 'purple', strokeWeight: 2,
-    radius1: 30, radius2: 70, npoints: 5, angle: 0, shape: 'star', lastSwitch: 0, heartScale: 1, heartScaleDirection: 1,
-    isMouseOver: function() { return dist(mouseX, mouseY, this.x, this.y) < this.radius2; },
+    x: 600,
+    y: 400,
+    w: 70,
+    h: 70,
+    colour: 'white',
+    stroke: 'purple',
+    strokeWeight: 2,
+    radius1: 30,
+    radius2: 70,
+    npoints: 5,
+    angle: 0,
+    shape: 'star',
+    lastSwitch: 0,
+    heartScale: 1,
+    heartScaleDirection: 1,
+
+    isMouseOver: function() {
+        let d = dist(mouseX, mouseY, this.x, this.y);
+        return d < this.radius2;
+    },
+
     draw: function() {
-        if (this.isMouseOver()) this.colour = "#B9AEFF"; else this.colour = 'white';
-        stroke(this.stroke); strokeWeight(this.strokeWeight); fill(this.colour); push(); translate(this.x, this.y);
+        if (this.isMouseOver()) {
+            this.colour = "#B9AEFF";
+        } else {
+            this.colour = 'white';
+        }
+
         if (this.shape === 'star') {
-            let angle = (Math.PI * 2) / this.npoints, halfAngle = angle / 2.0;
-            rotate(this.angle); beginShape();
-            for (let a = 0; a < Math.PI * 2; a += angle) {
-                let sx = cos(a) * this.radius2, sy = sin(a) * this.radius2;
+            let angle = TWO_PI / this.npoints;
+            let halfAngle = angle / 2.0;
+            stroke(this.stroke);
+            strokeWeight(this.strokeWeight);
+            fill(this.colour);
+            push();
+            translate(this.x, this.y);
+            rotate(this.angle);
+            beginShape();
+            for (let a = 0; a < TWO_PI; a += angle) {
+                let sx = cos(a) * this.radius2;
+                let sy = sin(a) * this.radius2;
                 vertex(sx, sy);
-                sx = cos(a + halfAngle) * this.radius1, sy = sin(a + halfAngle) * this.radius1;
+                sx = cos(a + halfAngle) * this.radius1;
+                sy = sin(a + halfAngle) * this.radius1;
                 vertex(sx, sy);
             }
             endShape(CLOSE);
-        } else {
-            scale(this.heartScale); beginShape();
-            for (let t = 0; t < Math.PI * 2; t += 0.01) vertex(16 * pow(sin(t), 3) * 2, - (13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)) * 2);
+            pop();
+        } else if (this.shape === 'heart') {
+            stroke(this.stroke);
+            strokeWeight(this.strokeWeight);
+            fill(this.colour);
+            push();
+            translate(this.x, this.y);
+            scale(this.heartScale);
+            beginShape();
+            for (let t = 0; t < TWO_PI; t += 0.01) {
+                let x = 16 * pow(sin(t), 3);
+                let y = -(13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t));
+                vertex(x * 2, y * 2);
+            }
             endShape(CLOSE);
+            pop();
         }
-        pop();
     },
     move: function() {
         this.angle += 0.006;
@@ -92,37 +166,32 @@ var star = {
         }
         if (this.shape === 'heart') {
             this.heartScale += 0.01 * this.heartScaleDirection;
-            if (this.heartScale > 1.2 || this.heartScale < 0.8) this.heartScaleDirection *= -1;
+            if (this.heartScale > 1.2 || this.heartScale < 0.8) {
+                this.heartScaleDirection *= -1;
+            }
         }
     }
 };
 
 function setup() {
-    createCanvas(windowWidth || 800, windowHeight || 600);
-    resetObjects();
+    createCanvas(windowWidth, windowHeight);
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth || 800, windowHeight || 600);
-    resetObjects();
-}
-
-function resetObjects() {
-    redBrick.w = height / 15; redBrick.h = height / 15;
-    blueBrick.w = height / 12; blueBrick.h = height / 12;
-    star.w = height / 10; star.h = height / 10;
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    if (isNaN(width) || isNaN(height)) return;
-    
     background("#FFF0F0");
-    
-    redBrick.draw(); redBrick.move();
-    blueBrick.draw(); blueBrick.move();
-    star.draw(); star.move();
-    
+    redBrick.draw();
+    redBrick.move();
+    blueBrick.draw();
+    blueBrick.move();
+    star.draw();
+    star.move();
+
     handleCollision(redBrick, blueBrick);
     handleCollisionStar(redBrick, star);
     handleCollisionStar(blueBrick, star);
 }
+
